6359d3ce7a14b5c46864ab080758cc2b25b913ab
Darth <redzed427@gmail.com>
2022-12-29 12:23:16 -0500

    Django_shop_lesson_3.3-3.4

--- store/products/views.py
+++ store/products/views.py
@@ -29,6 +29,24 @@ def products(request):
                 'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
                 'price': 3390,
                 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            },
+            {
+                'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
+                'name': 'Черный рюкзак Nike Heritage',
+                'price': 2340,
+                'description': 'Плотная ткань. Легкий материал.',
+            },
+            {
+                'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
+                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+                'price': 13590,
+                'description': 'Гладкий кожаный верх. Натуральный материал.',
+            },
+            {
+                'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
+                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+                'price': 2890,
+                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
             }
         ]
     }
