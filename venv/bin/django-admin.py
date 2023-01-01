c17ffafb88269b77f7954fecdb433722fc955e6d
Darth <an.9647873111@gmail.com>
2023-01-01 21:31:33 +0100

    Django_shop_lesson_3.5-3.6

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/nix/Projects/Projects_Parrot_Home/store-server/venv/bin/python3
+#!/home/nix/Projects/Clothing_store-/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
