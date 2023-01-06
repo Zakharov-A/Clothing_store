ea21392414cbe78ebe8b16f0c8d916fb23209a7e
Darth <an.9647873111@gmail.com>
2023-01-06 07:41:49 +0100

    Django_shop_lesson_4.6

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
     # --my apps--
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
