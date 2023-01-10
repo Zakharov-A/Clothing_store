df82cab1c086be654791504e6705a984197b94fb
Darth <redzed427@gmail.com>
2023-01-10 16:05:01 -0500

    Django_shop_lesson_4.8

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from users.models import User
+from users.forms import UserLoginForm
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    contex = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', contex)
 
 
 def registration(request):
