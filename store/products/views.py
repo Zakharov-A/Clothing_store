114a6d49e9e23cf3bc52411878497009d04801b1
Darth <an.9647873111@gmail.com>
2023-02-17 14:05:23 -0500

    Django_shop_lesson_7.4_part_2

--- store/products/views.py
+++ store/products/views.py
@@ -19,11 +19,13 @@ class IndexView(TemplateView):
 class ProductsListView(ListView):
     model = Product
     template_name = 'products/products.html'
+    paginate_by = 3
 
     def get_queryset(self):
         queryset = super(ProductsListView, self).get_queryset()
         category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id) if category_id else queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductsListView, self).get_context_data()
         context['title'] = 'Store - Каталог'
